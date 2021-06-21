1f2042591ee9d78da32584d42cd1ac4cd3279a6b
ak-047 <agrimakhanna5@gmail.com>
2021-06-21 13:42:54 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -15,8 +15,9 @@ var itemBeingEdited = {
 var itemList = {};
 count.textContent = 0;
 var correspondingDom = {};
-//click event listeners for edit and delete
+
 var handleClickEvent = (item) => {
+  //click event for  buttons of each item at the time of creating that item
   var editBtn = item.querySelector("#edit");
   var delBtn = item.querySelector("#delete");
 
@@ -24,7 +25,6 @@ var handleClickEvent = (item) => {
     //access name and quantity inside eventlistener otherwise values won't update
     var name = item.querySelector(".itemName").textContent;
     var quantity = item.querySelector(".itemQuantity").textContent;
-
     add_edit.classList.add("edit");
     cancel.classList.add("editActive");
 
@@ -43,29 +43,33 @@ var handleClickEvent = (item) => {
     list.removeChild(item);
     delete correspondingDom[name];
     delete itemList[name];
-    localStorage.itemList = JSON.stringify(itemList);
+    localStorage.itemsList = JSON.stringify(itemList);
     reset();
   });
 };
 
-if (localStorage.itemList != undefined)
-  itemList = JSON.parse(localStorage.itemList);
+if (localStorage.itemsList != undefined)
+  itemList = JSON.parse(localStorage.itemsList);
 
 for (var key in itemList) {
   if (itemList[key].dom && itemList[key]["dom"] != "") {
-    list.innerHTML += itemList[key]["dom"];
+    var item = document.createElement("li");
+    item.classList.add("item");
+    item.innerHTML = itemList[key]["dom"];
+    list.appendChild(item);
+    correspondingDom[key] = item;
     count.textContent++;
+    handleClickEvent(item);
+    //list.innerHTML+=    : this not only adds new content
+    //but also re - adds the existing innerHTML so click events are lost
+    // therefore append child
   } else delete itemList[key];
-  // console.log(list.lastElementChild);
 }
-list.childNodes.forEach((item) => {
-  var name = item.querySelector(".itemName").textContent;
-  correspondingDom[name] = item;
-  handleClickEvent(correspondingDom[name]);
-});
-localStorage.itemList = JSON.stringify(itemList);
+
+localStorage.itemsList = JSON.stringify(itemList);
 
 function createItem(name, quantity) {
+  //return node so that querySelector can be used
   var item = document.createElement("li");
   item.classList.add("item");
   item.innerHTML = `<div  class="details">
@@ -96,29 +100,35 @@ function reset() {
 form.addEventListener("submit", (event) => {
   var updated = 0;
   if (itemBeingEdited.status != false) {
+    //edit active
+
     if (inputName.value == itemBeingEdited.name) {
+      // only quantity update
       correspondingDom[inputName.value].querySelector(
         ".itemQuantity"
       ).innerHTML = inputQuantity.textContent;
 
       itemList[inputName.value]["dom"] =
-        correspondingDom[inputName.value].outerHTML;
+        correspondingDom[inputName.value].innerHTML;
 
       itemList[inputName.value]["quantity"] = inputQuantity.textContent;
       console.log(correspondingDom[inputName.value]);
-      localStorage.itemList = JSON.stringify(itemList);
+      localStorage.itemsList = JSON.stringify(itemList);
 
       updated = 1;
     } else {
+      //item name changed
       list.removeChild(correspondingDom[itemBeingEdited.name]);
       count.textContent--;
       delete correspondingDom[itemBeingEdited.name];
       delete itemList[itemBeingEdited.name];
-      localStorage.itemList = JSON.stringify(itemList);
+      localStorage.itemsList = JSON.stringify(itemList);
     }
   }
   if (!updated) {
+    //add item or item name changed
     if (itemList.hasOwnProperty(inputName.value)) {
+      //name already exists
       var oldQuantity =
         correspondingDom[inputName.value].querySelector(
           ".itemQuantity"
@@ -129,31 +139,32 @@ form.addEventListener("submit", (event) => {
       itemList[inputName.value]["quantity"] =
         inputQuantity.textContent - 0 + (oldQuantity - 0);
       itemList[inputName.value]["dom"] =
-        correspondingDom[inputName.value].outerHTML;
+        correspondingDom[inputName.value].innerHTML;
 
-      localStorage.itemList = JSON.stringify(itemList);
+      localStorage.itemsList = JSON.stringify(itemList);
 
       updated = 1;
       //console.log(inputQuantity.value);
     }
 
     if (!updated) {
+      //new item added
       var item = createItem(inputName.value, inputQuantity.textContent);
       list.appendChild(item);
       handleClickEvent(item);
 
       correspondingDom[inputName.value] = item;
       itemList[inputName.value] = {};
-      itemList[inputName.value]["dom"] = item.outerHTML;
+      itemList[inputName.value]["dom"] = item.innerHTML; //li tag not included
       itemList[inputName.value]["quantity"] = inputQuantity.textContent;
 
       count.textContent++;
 
-      localStorage.itemList = JSON.stringify(itemList);
+      localStorage.itemsList = JSON.stringify(itemList);
     }
   }
   reset();
-  //console.log(item);
+
   event.preventDefault();
 });
 
@@ -161,14 +172,14 @@ document.addEventListener(
   "click",
   function (event) {
     var btn = event.target.closest("button");
-    //console.log(event.target);
+
     if (btn != null) {
       if (btn.id == "deleteAll") {
         list.innerHTML = "";
         console.log(list);
         itemList = {};
         correspondingDom = {};
-        localStorage.itemList = JSON.stringify(itemList);
+        localStorage.itemsList = JSON.stringify(itemList);
         count.textContent = "0";
 
         reset();
